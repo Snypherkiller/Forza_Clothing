c057fd2199ed96700d73b6fe96ea4e1d36312d80
SnypherKiller <dnimtharu@gmail.com>
2025-06-20 00:00:08 +0530

    FOR-2 changes updated

--- Frontend/Forza_clothing/src/App.jsx
+++ Frontend/Forza_clothing/src/App.jsx
@@ -1,3 +1,6 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Home from "./pages/Home"
 
 
 function App() {
@@ -6,7 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/" element={<Home/>}></Route>
       </Routes>
     </BrowserRouter>
 
