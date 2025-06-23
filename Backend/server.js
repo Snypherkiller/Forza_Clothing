359b6723f5e32c69de7c1654aa893b2f9c4a7a66
SnypherKiller <dnimtharu@gmail.com>
2025-06-24 00:50:03 +0530

    Main updated

--- Backend/server.js
+++ Backend/server.js
@@ -5,5 +5,16 @@ const mongoose= require("mongoose");
 
 const app = express();
 const PORT=process.env.PORT // 3000;
+const MONGODB_URL=process.env.MONGODB_URL;
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Database connected successfully"+"and server is running on port"+PORT)
+    })
+})
+.catch((err)=>{
+    console.log("Database connection failed",err);
+})
 
 
