3ccd0b1b259646ec3590b12598ce5a041a4da0c4
SnypherKiller <dnimtharu@gmail.com>
2025-06-25 20:24:41 +0530

    FOR-10 User model Updated

--- Backend/controllers/users.controller.js
+++ Backend/controllers/users.controller.js
@@ -24,3 +24,25 @@ const getUserById = async (req,res)=>{
     }
 }
 
+const getAllUsers =async (req,res)=>{
+    try {
+        const users=await User.find();
+        if(!users || users.length === 0){
+            return res.status(404).json({message:"No users found"})
+        }
+        else{
+            res.status(200).json({message:"Users fetched successfully",users})
+        }
+    } catch (error) {
+        res.ststus(500).json ({message:"Something went wrong while fetching users",error})
+    }
+}
+
+const updateUser= async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
--- Backend/models/users.model.js
+++ Backend/models/users.model.js
@@ -2,6 +2,8 @@ const mongoose = require ("mongoose")
 
 const userSchema= new mongoose.Schema({
     name :{ type:String , require:true},
+    userName:{type:String,require:true,unique:true},
+    role:{ type:String , require:true,enum:["admin","user"]},
     email:{ type:String , require:true},
     address:{ type:String},
     phoneNumber:{ type:String },
