6d5f44e5dff2a02c32007f8170543c79dc5abfea
Kdisss <kalana545908@gmail.com>
2025-06-26 23:02:32 +0530

    FOR-2 updated

--- Frontend/Forza_clothing/src/pages/Home.jsx
+++ Frontend/Forza_clothing/src/pages/Home.jsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import ProductCard from '../components/ProductCard';
+import React from 'react'
 
 const Home = () => {
-    const [products,setProducts]=useState([]);
-
-    useEffect(()=>{
-        axios.get()
-        .then(res => setProducts(res.data))
-        .catch(err=>console.log(err))
-
-    },[]);
   return (
-    <div className="p-4">
-      <div className="text-2xl font-bold mb-4">Featured Products</div>
-      <dv classname="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {products.map((p, i) => (
-          <ProductCard key={i} product={p} />
-        ))}
-      </dv>
+    <div>
+      Hello from home
     </div>
-  );
+  )
 }
 
 export default Home
